54aca27a23964b1767e6bd302ff9f71cffc88e8a
Núbia da Fonseca <nubia.nfs@gmail.com>
2023-03-18 14:44:09 -0300

    Calculadora de porcentagem

---
+++ src/components/Teste.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+const Teste = () => {
+  return (
+    <View>
+      <Text>Teste</Text>
+    </View>
+  );
+};
+
+export default Teste;
+
+const styles = StyleSheet.create({});
